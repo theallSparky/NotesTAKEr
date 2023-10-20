4c520895c4f10a27b93bce8625c61543eca36f17
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-10-20 17:12:30 -0500

    Added a show and edit page

--- NotesTAKEr/src/App.tsx
+++ NotesTAKEr/src/App.tsx
@@ -6,6 +6,11 @@ function App() {
     <Routes>
       <Route path="/" element={<h1>Hello there, and welcome to NotesTAKEr!</h1>} />
       <Route path="/author" element={<h1>Welcome to the about author page!</h1>} />
+      <Route path="/new" element={<h1>New</h1> } />
+      <Route path="/:id">
+        <Route index element={<h1>Show</h1>} />
+        <Route path="edit" element={<h1>Edit</h1>} />
+      </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
