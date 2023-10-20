b7be1ba1ce3e05c527e4d3d9f430f85691f9a022
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-10-20 16:01:37 -0500

    Created a path to redirect unknown paths back to home

--- NotesTAKEr/src/App.tsx
+++ NotesTAKEr/src/App.tsx
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
-import { Route, Routes } from 'react-router-dom'
+import { Route, Routes, Navigate } from 'react-router-dom'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<h1>Hello there, and welcome to NotesTAKEr!</h1>} />
       <Route path="/author" element={<h1>Welcome to the about author page!</h1>} />
+      <Route path="*" element={<Navigate to="/" />} />
     </Routes>
   )
 }
