724e34ad51de684b7d50bf456da526f4d0faa48a
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-10-20 15:17:09 -0500

    Renamed title, added homepage and author page

--- NotesTAKEr/src/App.tsx
+++ NotesTAKEr/src/App.tsx
@@ -1,8 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
   return (
-    <h1>Hello there, and welcome to NotesTAKEr!</h1>
+    <Routes>
+      <Route path="/" element={<h1>Hello there, and welcome to NotesTAKEr!</h1>} />
+      <Route path="/author" element={<h1>Welcome to the about author page!</h1>} />
+    </Routes>
   )
 }
 
