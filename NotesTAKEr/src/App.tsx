e29135cba6f760cbbd307ff0351f7530cef07132
Harrison Sparks <harrisonsparks.1999@gmail.com>
2023-10-23 15:49:07 -0500

    Added react-select, made noteform and newnote

--- NotesTAKEr/src/App.tsx
+++ NotesTAKEr/src/App.tsx
@@ -1,18 +1,22 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { Container } from 'react-bootstrap'
 import { Route, Routes, Navigate } from 'react-router-dom'
+import { NewNote } from './NewNote'
 
 function App() {
   return (
+    <Container className="my-4">
     <Routes>
       <Route path="/" element={<h1>Hello there, and welcome to NotesTAKEr!</h1>} />
       <Route path="/author" element={<h1>Welcome to the about author page!</h1>} />
-      <Route path="/new" element={<h1>New</h1> } />
+      <Route path="/new" element={<NewNote />} />
       <Route path="/:id">
         <Route index element={<h1>Show</h1>} />
         <Route path="edit" element={<h1>Edit</h1>} />
       </Route>
       <Route path="*" element={<Navigate to="/" />} />
     </Routes>
+    </Container>
   )
 }
 
---
+++ NotesTAKEr/src/NewNote.tsx
@@ -0,0 +1,10 @@
+import { NoteForm } from './NoteForm'
+
+export function NewNote() {
+    return(
+        <div>
+            <h1 className="mb-4">Hi</h1>
+            <NoteForm />
+        </div>
+    )
+}
